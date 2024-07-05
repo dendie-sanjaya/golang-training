f6ec1474432b1d435a9bc24d556e006347f71987
Dendie <dendie.sanjaya@gmail.com>
2024-07-05 11:30:10 +0700

    grpc crud

---
+++ pertemuan-ke-12-crud-grpc/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net"
+	grpcHandler "praisindo/handler/grpc"
+	pb "praisindo/proto/user_service/v1"
+	"praisindo/repository/postgres_gorm"
+	"praisindo/service"
+
+	"google.golang.org/grpc"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+	// setup gorm connection
+	dsn := "postgresql://postgres:12345678@localhost:5432/Golang"
+	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		log.Fatalln(err)
+	}
+	// setup service
+
+	// uncomment to use postgres gorm
+	userRepo := postgres_gorm.NewUserRepository(gormDB)
+	userService := service.NewUserService(userRepo)
+	//userHandler := ginHandler.NewUserHandler(userService)
+	userHandler := grpcHandler.NewUserHandler(userService)
+
+	// Run the grpc server
+	grpcServer := grpc.NewServer()
+	pb.RegisterUserServiceServer(grpcServer, userHandler)
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	log.Println("Running grpc server in port :50051")
+	_ = grpcServer.Serve(lis)
+}
