92cfc3d1ea9f817d8634b5c31e36a76505594b67
Dendie <dendie.sanjaya@gmail.com>
2024-08-08 14:53:05 +0700

    feat: auto migration untuk ms-user

--- pertemuan-ke-20-assigment-5/micoservice-user/go.mod
+++ pertemuan-ke-20-assigment-5/micoservice-user/go.mod
@@ -9,6 +9,8 @@ require (
 	github.com/jackc/pgx/v5 v5.6.0
 	github.com/pashagolub/pgxmock/v2 v2.12.0
 	github.com/stretchr/testify v1.9.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
 	gorm.io/driver/postgres v1.5.9
 	gorm.io/gorm v1.25.10
 )
@@ -49,7 +51,5 @@ require (
 	golang.org/x/sys v0.21.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240528184218-531527333157 // indirect
-	google.golang.org/grpc v1.65.0 // indirect
-	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- pertemuan-ke-20-assigment-5/micoservice-user/main.go
+++ pertemuan-ke-20-assigment-5/micoservice-user/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
 	grpcHandler "praisindo/handler/grpc"
@@ -8,15 +9,64 @@ import (
 	"praisindo/repository/postgres_gorm"
 	"praisindo/service"
 
+	"praisindo/config"
+	"praisindo/entity"
+
 	"google.golang.org/grpc"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 func main() {
-	// setup gorm connection
-	dsn := "postgresql://postgres:12345678@localhost:5432/Golang"
+	// Setup gorm connection without selecting a database
+	dsn := "host=" + config.PostgresHost + " port=" + config.PostgressPort + " user=" + config.PostgresUser + " password=" + config.PostgresPassword + " sslmode=" + config.PostgresSSLMode
+	//dsn := "postgresql://postgres:password@postgres:5434/postgres?sslmode=disable"
+	fmt.Println(dsn)
 	gormDB, err := gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Println("Database connection established", config.PostgresHost, config.PostgressPort, config.PostgresSSLMode)
+	}
+
+	// Check if the database exists
+	var exists bool
+	err = gormDB.Raw("SELECT EXISTS(SELECT datname FROM pg_catalog.pg_database WHERE datname = ?)", config.PostgresDB).Scan(&exists).Error
+	if err != nil {
+		log.Fatalln(err)
+	} else {
+		log.Println("Database exists:", exists, config.PostgresDB)
+	}
+
+	// Create the database if it does not exist
+	if !exists {
+		err = gormDB.Exec("CREATE DATABASE " + config.PostgresDB).Error
+		if err != nil {
+			log.Fatalln(err)
+		} else {
+			log.Println("Database created successfully")
+		}
+	}
+
+	// Reconnect to the newly created database
+	dsn = "host=" + config.PostgresHost + " port=" + config.PostgressPort + " user=" + config.PostgresUser + " password=" + config.PostgresPassword + " dbname= " + config.PostgresDB + " sslmode=" + config.PostgresSSLMode
+	gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	// Migrate the schema
+	err = gormDB.AutoMigrate(&entity.User{})
+	if err != nil {
+		fmt.Println("Failed to migrate database schema user saldo:", err)
+	} else {
+		fmt.Println("Database schema migrated user saldo successfully")
+	}
+
+	// setup gorm connection
+	//dsn := "postgresql://postgres:12345678@localhost:5432/Golang"
+	dsn = config.PostgresStringConnection
+	gormDB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{SkipDefaultTransaction: true})
 	if err != nil {
 		log.Fatalln(err)
 	}
